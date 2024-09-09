054cbc9b1b9b285bf08af41875f8406dd0f55987
StamatisChr <stamatios.chrysinas@hashicorp.com>
2024-09-09 14:03:08 +0200

    added output for http replicated console

--- AWS/outputs.tf
+++ AWS/outputs.tf
@@ -20,5 +20,11 @@ output "ubuntu_ami_description" {
 
 output "connect_to_ec2_via_ssh" {
   description = "use the following command to connect to your ec2 instance with SSH."
-  value       = "Wait a few minutes. \n then use this command to connect with ssh \n ssh ubuntu@${aws_instance.tfe_instance.public_dns}"
+  value       = "Wait a few minutes. Then use this command to connect with ssh \n ssh ubuntu@${aws_instance.tfe_instance.public_dns}"
+}
+
+output "tfe_http_console" {
+  description = "The HTTP url to connect and continue TFE configuration"
+  value = "http://${var.my_tfe_dns_record}:8800"
+  
 }
